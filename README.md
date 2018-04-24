39cbce305b4fba8aa210fb7da6784f277e878b59
John Wang <jue@alumni.cmu.edu>
2018-04-23 22:00:36 -0400

    update: docker compose not working so skipping for now

--- README.md
+++ README.md
@@ -11,10 +11,19 @@ References
 
 # Run
 
+## Compose
 * `docker stack deploy -c docker-compose.yml prometheus`
 * Attach to a running client container and send requests to the other 
 containers to trigger some metrics
 * Go to localhost:9090 to see Prometheus console
+
+## Manual
+* `docker run -d prom-client`
+  * run this N times for N clients
+* `docker network inspect bridge` to get ip addresses of clients
+* update server/prometheus.yml with IP of clients
+* `docker-compose build` to create server image that targets clients
+* `docker run -d -p 9090:9090 prom-server`
 
 # Dependencies
 
--- client/src/prom-client.py
+++ client/src/prom-client.py
@@ -9,28 +9,36 @@ CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
 application = flask.Flask(__name__)
 
 # Create a metric to track time spent and requests made.
-REQUEST_TIME = prometheus_client.Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_TIME = prometheus_client.Summary(
+    'request_processing_seconds', 'Time spent processing request',
+    ['fn', 'route'])
+REQUEST_TIME_ENDPOINT = REQUEST_TIME.labels(fn='time_fn', route='/time-endpoint')
+REQUEST_TIME_FN = REQUEST_TIME.labels(fn='time_fn', route='/time-fn')
+
 
 @application.route('/metrics')
 def handle_prometheus_metrics():
     return flask.Response(
         prometheus_client.generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
+
 @application.route('/time-endpoint')
-@REQUEST_TIME.time()
+@REQUEST_TIME_ENDPOINT.time()
 def handle_time_endpoint():
     print('handle time endpoint - thread %s\n' % threading.get_ident())
     t = random.random()
     time.sleep(t)
     return 'time-endpoint: %d' % t
 
+
 @application.route('/time-fn')
+@REQUEST_TIME_FN.time()
 def handle_time_fn():
     print('handle time fn - thread %s\n' % threading.get_ident())
     t = random.random()
     return time_fn()
 
-@REQUEST_TIME.time()
+
 def time_fn():
     t = random.random()
     time.sleep(t)
